4228bbb4e7566413287b316b3967ec41ea97b78a
Administrator <Administrator@ThinkBook>
2024-08-05 15:53:21 +0900

    호

---
+++ DOZO/src/main/java/co/yedam/web/ProductControl.java
@@ -0,0 +1,28 @@
+package co.yedam.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.yedam.common.Control;
+import co.yedam.service.ProductService;
+import co.yedam.service.ProductServiceImpl;
+import co.yedam.vo.ProductVO;
+
+public class ProductControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		String prdName = req.getParameter("prdName");
+//		System.out.println(prdName);
+//		ProductService svc = new ProductServiceImpl();		
+//		ProductVO product = svc.getProduct(prdName);
+//
+//		req.setAttribute("product", product);
+
+		req.getRequestDispatcher("product/product.tiles").forward(req, resp); // 페이지 재지정
+	}
+
+}

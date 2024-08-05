9091fcd184b54934d420e3001420c1e22f8b493b
Administrator <Administrator@ThinkBook>
2024-08-05 15:01:46 +0900

    0805 lsy

---
+++ MinJun/src/main/java/co/yedam/service/ProductService.java
@@ -0,0 +1,10 @@
+package co.yedam.service;
+
+import java.util.List;
+
+import co.yedam.vo.ProductVO;
+
+public interface ProductService {
+//	List<ProductVO> productList();
+//	ProductVO getProduct(String prdName);
+} 
